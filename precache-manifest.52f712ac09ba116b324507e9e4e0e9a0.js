283adb7143574749997f0a4ef5ba11796e9b016f
GaurangDhorda <dg.dhorda@gmail.com>
2020-08-31 16:14:59 +0530

    Updates

---
+++ precache-manifest.52f712ac09ba116b324507e9e4e0e9a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3dbed1f1274297e8243758ccc469cf",
+    "url": "/ImageTextEdition/index.html"
+  },
+  {
+    "revision": "5a4267d080febf64d95e",
+    "url": "/ImageTextEdition/static/css/main.bdd8e4d7.chunk.css"
+  },
+  {
+    "revision": "1909f488614d754847d4",
+    "url": "/ImageTextEdition/static/js/2.1c23c4ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ImageTextEdition/static/js/2.1c23c4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4267d080febf64d95e",
+    "url": "/ImageTextEdition/static/js/main.13464721.chunk.js"
+  },
+  {
+    "revision": "cfd2426959b9a5b7b425",
+    "url": "/ImageTextEdition/static/js/runtime-main.ed8bf623.js"
+  }
+]);
